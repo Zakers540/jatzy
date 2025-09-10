26f55cb412c8590164ebdd5f1540ae0b6ed74d70
ZakersLaptop <nukl3430@gmail.com>
2025-09-10 13:29:16 +0200

    fixed rndURL

--- api/jatzy.py
+++ api/jatzy.py
@@ -3,9 +3,12 @@ import random
 def rndURL():
     i = 0
     s = ""
-    while(i < 10):
+    while(i < 6):
         rndCh = int(random.uniform(0, 1) * 127)
-        if(rndCh * 127 >= 32):
+        if(rndCh > 48 and rndCh < 57):
             s += chr(rndCh)
             i += 1
     return s
+
+for i in range(20):
+    print(rndURL())
