d56ffa94f38ee0b96278d8778618190c1dec4bbd
ZakersLaptop <nukl3430@gmail.com>
2025-09-10 11:11:24 +0200

    dynamic url

--- api/jatzy.py
+++ api/jatzy.py
@@ -1,1 +1,13 @@
 import random
+
+def rndURL():
+    i = 0
+    s = ""
+    while(i < 10):
+        rndCh = int(random.uniform(0, 1) * 127)
+        if(rndCh * 127 >= 32):
+            s += chr(rndCh)
+            i += 1
+    return s
+
+print(rndURL())
