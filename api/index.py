e2f8fe3118510cae68fc20a91fb9c063ea6de15e
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-09-10 17:19:48 +0200

    fixed error maybe

--- api/index.py
+++ api/index.py
@@ -3,12 +3,9 @@ import jatzy
 
 app = Flask(__name__)
 
-currentInstances = []
-
 @app.route("/api/opret")
 def gameInstance():
     instanceId = jatzy.rndURL()
-    currentInstances.append(instanceId)
     return redirect(f"/server/{instanceId}", code=302)
 
 @app.route("/api/tjek/<instanceId>")
