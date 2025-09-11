0906d87e29e48f7515435fb389102c3e9061d14b
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-09-11 11:36:09 +0200

    improved styling

--- api/index.py
+++ api/index.py
@@ -8,10 +8,9 @@ def rndURL():
     i = 0
     s = ""
     while(i < 6):
-        rndNr = int(random.uniform(0, 9)) + 48 
-        if(rndNr >= 48 and rndNr <= 57):
-            s += chr(rndNr)
-            i += 1
+        rndNr = int(random.uniform(0, 9)) + 48
+        s += chr(rndNr)
+        i += 1
     return s
 
 currentInstances = []
