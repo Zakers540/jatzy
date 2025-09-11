24d0cc14dde303c2080f11ee3e79aaae19488512
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-09-11 18:30:14 +0200

    improved styling

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,14 @@ export default function Home() {
     }, []);
   return (
       <main className="min-h-screen w-full flex flex-col pt-40 px-20 md:grid md:grid-cols-2 md:items-start">
-          <div className="flex flex-col items-center mt-8">
-            <h1 className="text-black/80 text-4xl font-semibold bg-white/80 rounded-md p-2">
+          <div className="flex flex-col items-center -mt-8 md:mt-8">
+            <div className="md:hidden flex justify-center mb-12 md:mt-0">
+                <BigDice diceNumber={diceNumber}/>
+            </div>
+            <h1 className="text-center text-black/80 text-4xl font-semibold bg-white/80 rounded-md p-2">
                 Har du også lyst til yatzy?
             </h1>
-            <p className="mt-2 text-md text-neutral-500 bg-white/60 rounded-md p-1">
+            <p className="text-center mt-2 text-md text-neutral-500 bg-white/60 rounded-md p-1">
                 Det har du selvfølgelig!
             </p>
             <a href="/api/opret" className="p-4 border-2 border-blue-500 rounded-2xl text-xl mt-8 text-black/80
@@ -26,7 +29,7 @@ export default function Home() {
                 Start et spil yatzy
             </a>
           </div>
-          <div className="flex justify-center">
+          <div className="hidden md:flex justify-center mt-24 md:mt-0">
               <BigDice diceNumber={diceNumber}/>
           </div>
       </main>
--- components/BigDice.tsx
+++ components/BigDice.tsx
@@ -20,68 +20,68 @@ export default function BigDice({ diceNumber }: DiceProps) {
         })
     }, [diceNumber])
     return (
-        <div className="flex justify-center items-center border rounded-lg w-64 h-64 bg-neutral-50">
+        <div className="flex justify-center items-center border rounded-md w-32 h-32 md:rounded-lg md:w-64 md:h-64 bg-neutral-50">
             {(() => {
                 switch (diceNumber) {
                     case 1:
                         return (
                             <div className="w-full h-full flex justify-center items-center">
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                             </div>
                         )
                     case 2:
                         return (
-                            <div className="w-full h-full flex justify-between items-center flex-col p-8 py-12">
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
+                            <div className="w-full h-full flex justify-between items-center flex-col p-4 md:p-8 py-6 md:py-12">
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                             </div>
                         )
                     case 3:
                         return (
-                            <div className="w-full h-full flex flex-col justify-between p-8">
+                            <div className="w-full h-full flex flex-col justify-between p-4 md:p-8">
                                 <div className="flex justify-start">
-                                    <div className="dot w-12 h-12 bg-black rounded-full" />
+                                    <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                                 </div>
                                 <div className="flex justify-center">
-                                    <div className="dot w-12 h-12 bg-black rounded-full" />
+                                    <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                                 </div>
                                 <div className="flex justify-end">
-                                    <div className="dot w-12 h-12 bg-black rounded-full" />
+                                    <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                                 </div>
                             </div>
                         )
                     case 4:
                         return (
-                            <div className="w-full h-full grid grid-cols-2 gap-8 p-8 py-6 place-items-center">
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
+                            <div className="w-full h-full grid grid-cols-2 gap-4 p-4 py-3 md:gap-8 md:p-8 md:py-6 place-items-center">
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                             </div>
                         )
                     case 5:
                         return (
-                            <div className="w-full h-full grid grid-cols-3 grid-rows-3 p-8 py-6 place-items-center">
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
+                            <div className="w-full h-full grid grid-cols-3 grid-rows-3 p-4 py-3 md:p-8 md:py-6 place-items-center">
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                                 <div />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                                 <div />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                                 <div />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                                 <div />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                             </div>
                         )
                     case 6:
                         return (
-                            <div className="w-full h-full grid grid-cols-2 gap-y-8 p-8 py-6 place-items-center">
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
-                                <div className="dot w-12 h-12 bg-black rounded-full" />
+                            <div className="w-full h-full grid grid-cols-2 gap-y-4 md:gap-y-8 md:p-8 p-4 py-3 md:py-6 place-items-center">
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
+                                <div className="dot w-6 h-6 md:w-12 md:h-12 bg-black rounded-full" />
                             </div>
                         )
                 }
