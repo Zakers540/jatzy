0adbdffee3ef4dadc1240d5e6bfe113af14b1ca5
ZakersLaptop <nukl3430@gmail.com>
2025-09-11 13:56:44 +0200

    added time tjek & user tjek

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, jsonify
 from flask_cors import CORS
-import random
+import datetime
+from random import uniform
 
 app = Flask(__name__)
 CORS(app)
@@ -8,17 +9,21 @@ def rndURL():
     i = 0
     s = ""
     while(i < 6):
-        rndNr = int(random.uniform(0, 9)) + 48
+        rndNr = int(uniform(0, 9)) + 48
         s += chr(rndNr)
         i += 1
     return s
 
-currentInstances = []
+players = []
+currentInstances = [] # evenIndex = gameInstance, unevenIndex = instanceTime
+
 players = ["Torben", "test"]
 @app.route("/api/opret")
 def gameInstance():
     instanceId = rndURL()
     currentInstances.append(instanceId)
+    day = datetime.datetime.date()
+    currentInstances.append(day)
     return redirect(f"/server/{instanceId}", code=302)
 
 @app.route("/api/tjek/<instanceId>")
@@ -32,3 +37,23 @@ def get_data(instanceId):
         "instanceId": instanceId,
         "players": players
     })
+
+@app.route("/api/tjek/<instanceId>/time")
+def serverTime(instanceId):
+    if instanceId in currentInstances:
+        index = currentInstances.index(instanceId)
+        currentDay = datetime.datetime.date()
+        if currentDay - currentInstances[index + 1] >= 7:
+            return redirect(f"/server/{instanceId}", code=302)
+        else:
+            currentInstances.remove(index)
+            currentInstances.remove(index + 1)
+    return jsonify({"exists":False})
+
+
+@app.route("/api/tjek/<instanceId>/<name>")
+def name_exists(instanceId, name):
+    if name in players:
+        return jsonify({"exists": True})
+    players.append(name)
+    return jsonify({"exists":False})
