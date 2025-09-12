87a85421753ac7db0fb22e5267c9241ab7dc02d7
ZakersLaptop <nukl3430@gmail.com>
2025-09-12 11:29:28 +0200

    added supabase database

--- api/index.py
+++ api/index.py
@@ -1,7 +1,13 @@
 from flask import Flask, redirect, jsonify
 from flask_cors import CORS
+import os
+from supabase import create_client, Client
 from time import localtime
 from random import uniform
+
+url: str = os.environ.get("https://whaiekidzkrnqiyykhjr.supabase.co")
+key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoYWlla2lkemtybnFpeXlraGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NjE0NTQsImV4cCI6MjA3MzIzNzQ1NH0.luGyAzMASyma0kYS2n8kZs6MUrzEyneJTuM3LbX3AXc")
+supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
 CORS(app)
@@ -21,14 +27,13 @@ players = ["Torben", "test"]
 @app.route("/api/opret")
 def gameInstance():
     instanceId = rndURL()
-    currentInstances.append(instanceId)
     instanceStartTime = [localtime().tm_mday, localtime().tm_mon]
-    currentInstances.append(instanceStartTime)
+    response = (supabase.table("server").insert({"instanceId": instanceId, "timeCreated": instanceStartTime}))
     return redirect(f"/server/{instanceId}", code=302)
 
 @app.route("/api/tjek/<instanceId>")
 def instance_exists(instanceId):
-    exists = instanceId in currentInstances
+    exists = supabase.table("server").select("*").in_("instanceId", [str(instanceId)])
     return jsonify({"exists": exists})
 
 @app.route("/api/data/<instanceId>", methods=["GET"])
@@ -40,12 +45,13 @@ def get_data(instanceId):
 
 @app.route("/api/tjek/<instanceId>/time")
 def serverTime(instanceId):
-    if instanceId in currentInstances:
-        index = currentInstances.index(instanceId)
+    i = 1
+    while supabase.table("server").select("instanceId").contains("id", [str(i)]) != None:
+        time = supabase.table("server").select("instanceId").contains("timeCreated",["timeCreated"])
         currentDate = localtime().tm_mday
         currentMonth = localtime().tm_mon
-        instanceMonth = currentInstances[index + 1][1]
-        instanceDate = currentInstances[index + 1][0]
+        instanceMonth = time[1]
+        instanceDate = time[0]
         if currentMonth > instanceMonth or (currentMonth == instanceMonth and currentDate < instanceDate):
             instanceTime = (currentMonth - instanceMonth) * 30 + (instanceDate - currentDate)
         else:
@@ -53,8 +59,7 @@ def serverTime(instanceId):
         if instanceTime >= 7:
             return redirect(f"/server/{instanceId}", code=302)
         else:
-            currentInstances.remove(index)
-            currentInstances.remove(index + 1)
+            dltCollum = supabase.table("server").delete().eq("id", i).execute()
     return jsonify({"exists":False})
 
 
