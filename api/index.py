3b078f4bb92d65bc6fe78bafd323662d199d45af
Zakers540 <nukl3430@gmail.com>
2025-09-14 00:06:40 +0200

    added users table

--- api/index.py
+++ api/index.py
@@ -5,6 +5,12 @@ from supabase import create_client, Client
 from datetime import date
 from time import localtime
 from random import uniform
+import base64
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto import Random
+
+cryptKey = b"dvawdbabdkawje802v354u0ba+d23u82nmyvn30cn2039xm234vn7"
 
 url: str = os.environ.get('DATABASE_URL')
 key: str = os.environ.get('DATABASE_KEY')
@@ -21,6 +27,36 @@ def rndURL():
         i += 1
     return s
 
+def cleanUsername(name):
+    nameLen = len(name)
+    i = 0
+    while i < nameLen
+        if i = 0:
+            continue
+        if name[i] == ' ' and name[i - 1] == ' '
+            del name[i]
+    return name.strip()
+
+def encrypt(key, source, encode=True):
+    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
+    IV= Random.new().read(AES.block_size)  # generate IV
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
+    padding = AES.block_size - len(source) % AES.block_size  # calculate needed padding
+    source += bytes([padding]) * padding  # Python 2.x: source += chr(padding) * padding
+    data = IV + encryptor.encrypt(source)  # store the IV at the beginning and encrypt
+    return base64.b64encode(data).decode("latin-1") if encode else data
+
+def decrypt(key, source, decode=True):
+    if decode:
+        source = base64.b64decode(source.encode("latin-1"))
+    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
+    IV = source[:AES.block_size]  # extract the IV from the beginning
+    decryptor = AES.new(key, AES.MODE_CBC, IV)
+    data = decryptor.decrypt(source[AES.block_size:])  # decrypt
+    padding = data[-1]  # pick the padding value from the end; Python 2.x: ord(data[-1])
+    if data[-padding:] != bytes([padding]) * padding:  # Python 2.x: chr(padding) * padding
+        raise ValueError("Invalid padding...")
+    return data[:-padding]  # remove the padding
 
 players = []
 currentInstances = [] # evenIndex = gameInstance, unevenIndex = instanceTime[date, month]
@@ -76,6 +112,9 @@ def name_exists(instanceId, name):
     if instanceId in currentInstances:
         if name in players:
             return jsonify({"exists": True})
-        players.append(name)
         return jsonify({"exists":False})
     return jsonify({"instanceExists":False})
+
+@app.route("/api/tilfoej/{instanceId}/{user}/{password}")
+def addUser(instanceId, user, password):
+    user = (supabase.table("users").insert({"username": cleanUsername(user), "password": encrypt(cryptKey, password), "gameInstance": instanceId}))
